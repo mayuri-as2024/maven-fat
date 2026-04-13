63bb5185fdff8e2626702054703e005306c5b5d6
mayuri-as2024 <mayuri.as2024@vitstudent.ac.in>
2026-04-13 12:55:23 +0530

    Add files via upload

--- maven/App.java
+++ maven/App.java
@@ -1,9 +1,25 @@
+package maven;
+
 public class App {
- public static void main(String[] args) throws Exception {
- System.out.println("Java CI/CD Application Started...");
- while (true) {
- Thread.sleep(5000);
- System.out.println("Hello from Java CI/CD Pipeline!");
- }
- }
+    public static void main(String[] args) {
+        int n = 10; // Number of terms to generate
+        int firstTerm = 0, secondTerm = 1;
+        
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(firstTerm + ", ");
+
+            // Compute the next term
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
+
+    // Method for JUNIT testing
+    public static int getFibonacci(int n) {
+        if (n <= 1) return n;
+        return getFibonacci(n - 1) + getFibonacci(n - 2);
+    }
 }
